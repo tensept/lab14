dda4a9fc193af77a9315fd361eb36074da250555
tensept <vankaew58@gmail.com>
2023-01-13 15:56:48 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,25 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double d[], int N, double a[] ){
+    double max = d[0],min = d[0];
+    double sum = 0, sum_sd = 0, sum_gm = 1, sum_hm = 0;
+    for(int i=0;i<N;i++){
+        sum += d[i];
+        a[0] = sum/N;
+        sum_sd += pow(d[i],2);
+        a[1] = sqrt(((sum_sd)/N)-pow((sum/N),2));
+        sum_gm *= d[i];
+        a[2] = pow(sum_gm,(1.0/N));
+        sum_hm += 1/d[i];
+        a[3] = N/sum_hm;   
+        for(int j=1;j<N;j++){
+        if(d[j]>max) max = d[j];
+        if(d[j]<min) min = d[j];
+        }
+        a[4] = max;
+        a[5] = min;
+    }
+    
+}
